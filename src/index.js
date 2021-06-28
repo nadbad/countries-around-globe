2ee5ca69091898ec236c9ad3530b6b802775bd05
Nadeem <nadbadvlogs@gmail.com>
2021-06-28 09:10:43 +0200

    refactored the code a bit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './sass/sass.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import CountryDetail from './components/mainPage/mainPageSub/countryDetail';
+import CountryDetail from './components/countriesDetails/countryDetail';
 
 const store = configureStore();
 
